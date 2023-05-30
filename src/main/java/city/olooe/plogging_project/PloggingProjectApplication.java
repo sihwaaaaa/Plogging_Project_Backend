dd915e939d208681a3c6b3a839408483d3957046
eungyeong2 <eungyeong86@gmail.com>
2023-05-30 15:50:36 +0900

    init

---
+++ src/main/java/city/olooe/plogging_project/PloggingProjectApplication.java
@@ -0,0 +1,13 @@
+package city.olooe.plogging_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloggingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PloggingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/city/olooe/plogging_project/PloggingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package city.olooe.plogging_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloggingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
