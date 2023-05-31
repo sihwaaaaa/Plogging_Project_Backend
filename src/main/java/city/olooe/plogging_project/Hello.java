d4f23524cb0d610a1daae530da29cce3b8923c75
yeonjae97 <yeonjae978@gmail.com>
2023-05-31 17:59:53 +0900

    연재 커밋

--- src/main/java/city/olooe/plogging_project/Hello.java
+++ src/main/java/city/olooe/plogging_project/Hello.java
@@ -3,5 +3,6 @@ package city.olooe.plogging_project;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("박연재");
     }
 }
