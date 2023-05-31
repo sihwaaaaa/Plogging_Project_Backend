7f3e56872191ebab63c7ec9442760338ee2dfb72
developerjini <i5veagl@gmail.com>
2023-05-31 18:02:23 +0900

    추가 syso 작성

--- src/main/java/city/olooe/plogging_project/Hello.java
+++ src/main/java/city/olooe/plogging_project/Hello.java
@@ -3,5 +3,6 @@ package city.olooe.plogging_project;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("김성진이 작성");
     }
 }
