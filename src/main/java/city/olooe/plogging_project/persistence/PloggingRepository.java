4af88cf6ee1bd8970ca079efcef288c087315dd5
이시화 <tlghk9898@gmail.com>
2023-06-01 21:47:35 +0900

    플로깅 관련 엔티티 레파지토리 최초작성

---
+++ src/main/java/city/olooe/plogging_project/persistence/PloggingRepository.java
@@ -0,0 +1,18 @@
+package city.olooe.plogging_project.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import city.olooe.plogging_project.model.PloggingEntity;
+
+@Repository
+/*
+ * @author : 이시화
+ * 
+ * @date: 23.06.01
+ * 
+ * @brief: PloggingEntity jpa 구현체
+ */
+public interface PloggingRepository extends JpaRepository<PloggingEntity, String> {
+
+}
---
+++ src/main/java/city/olooe/plogging_project/persistence/UserMapRepository.java
@@ -0,0 +1,18 @@
+package city.olooe.plogging_project.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import city.olooe.plogging_project.model.UserMapEntity;
+
+@Repository
+/*
+ * @author : 이시화
+ * 
+ * @date: 23.06.01
+ * 
+ * @brief: UserMapEntity jpa 구현체
+ */
+public interface UserMapRepository extends JpaRepository<UserMapEntity, String> {
+
+}
