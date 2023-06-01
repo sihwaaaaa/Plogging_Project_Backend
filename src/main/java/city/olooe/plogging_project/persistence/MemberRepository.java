785cce2ed7a8e1c1067f5aa2a3a523109c55aaad
yeonjae97 <yeonjae978@gmail.com>
2023-06-02 02:09:37 +0900

    회원 dto, entity, repository 생성 및 repository 단위 테스트

--- src/main/java/city/olooe/plogging_project/persistence/MemberRepository.java
+++ src/main/java/city/olooe/plogging_project/persistence/MemberRepository.java
@@ -1,4 +1,17 @@
 package city.olooe.plogging_project.persistence;
 
-public class MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import city.olooe.plogging_project.model.MemberEntity;
+
+/**
+ * @author: 박연재
+ * @date: 2023.06.01
+ * @brief: MemberEntity의 jpa 인터페이스
+ */
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+  MemberEntity findByUserIdAndPassword(String userId, String password);
 }
