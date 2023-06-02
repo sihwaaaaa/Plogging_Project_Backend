8bcf40c89912bde56d447c1aab1bcdb0317510b0
이시화 <tlghk9898@gmail.com>
2023-06-02 10:16:10 +0900

    test명 정함

---
+++ src/test/java/city/olooe/plogging_project/persistence/MapRepositoryTest.java
@@ -0,0 +1,9 @@
+package city.olooe.plogging_project.persistence;
+
+import org.junit.jupiter.api.DisplayName;
+
+@Test
+@DisplayName("맵 저장시 경유지도 같이 잘 저장되는지 테스트")
+public class MapRepositoryTest {
+
+}
