1ea335f66b46fcea1607222944aee95777a1f5b5
yeonjae97 <yeonjae978@gmail.com>
2023-06-08 04:29:28 +0900

    소셜 로그인 백엔드 설정

--- src/main/java/city/olooe/plogging_project/security/TokenProvider.java
+++ src/main/java/city/olooe/plogging_project/security/TokenProvider.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import city.olooe.plogging_project.model.MemberEntity;
@@ -44,5 +45,17 @@ public class TokenProvider {
         .getBody();
 
     return claims.getSubject();
+  }
+
+  public String create(Authentication authentication) {
+    ApplicationOAuth2User userPrincipal = (ApplicationOAuth2User) authentication.getPrincipal();
+    Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+    return Jwts.builder()
+      .setSubject(userPrincipal.getName())
+      .setIssuedAt(new Date())
+      .setExpiration(expiryDate)
+        .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+      .compact();
   }
 }
