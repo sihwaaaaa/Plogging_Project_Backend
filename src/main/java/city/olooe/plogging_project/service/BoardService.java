5d83df617e6e27710667a83432843f3ac4697e77
developerjini <i5veagl@gmail.com>
2023-06-08 18:12:33 +0900

    BoardRepositoryTest 완료(CRUD) 파일 첨부 관련 Entity 생성

--- src/main/java/city/olooe/plogging_project/service/BoardService.java
+++ src/main/java/city/olooe/plogging_project/service/BoardService.java
@@ -1,5 +1,6 @@
 package city.olooe.plogging_project.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -57,7 +58,7 @@ public class BoardService {
     BoardEntity boardEntity = boardRepository.findById(bno)
         .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
 
-    boardEntity.update(boardUpdateDTO.getTitle(), boardUpdateDTO.getContent());
+    boardEntity.update(boardUpdateDTO.getTitle(), boardUpdateDTO.getContent(), new Date());
 
     return bno;
   }
