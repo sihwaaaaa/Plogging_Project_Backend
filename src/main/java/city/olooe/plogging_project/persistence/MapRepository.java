82bad668e45e11b7804d59e53cfa84b4e54566db
이시화 <tlghk9898@gmail.com>
2023-06-27 09:53:51 +0900

    추천경로 리스트 무한스크롤링 처리 및 검색시 페이징 삭제

--- src/main/java/city/olooe/plogging_project/persistence/MapRepository.java
+++ src/main/java/city/olooe/plogging_project/persistence/MapRepository.java
@@ -2,6 +2,7 @@ package city.olooe.plogging_project.persistence;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -22,5 +23,6 @@ import javax.persistence.EntityManager;
  */
 @Repository
 public interface MapRepository extends JpaRepository<MapEntity, Long> {
-  Page<MapEntity> findByAddrContainingOrCourseNameContainingOrCourseDetailContaining(String courseDetail,String courseName,String addr, Pageable pageable);
+  List<MapEntity> findByAddrContainingOrCourseNameContainingOrCourseDetailContaining(String courseDetail,String courseName,String addr);
+   Slice<MapEntity> findByMapNoLessThan(Long mapNo,Pageable pageable);
 }
